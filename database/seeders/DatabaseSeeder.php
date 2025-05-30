18b5fd88a25aecea86af942cb70e760f8341fadc
Ahda Firly Barori <51354643+adhfry@users.noreply.github.com>
2025-05-30 08:34:38 +0700

    update/add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Mode;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -22,6 +23,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
+            Mode::class,
             RoleSeeder::class,
             UserSeeder::class,
         ]);
